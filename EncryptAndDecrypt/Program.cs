076081e958fd70de793c1ac967e4e53e7f90031c
Abdullah Saleh AlSaleh <abdalla.aa5516@hotmail.com>
2022-01-05 10:54:11 +0300

    Add project files.

---
+++ EncryptAndDecrypt/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncryptAndDecrypt
+{
+    internal class Program
+    {
+        public static string Encrypt(string inputText)
+        {
+            //string encryptionkey = "SAUW193BX628TD57";
+            string encryptionkey = "SAUW193BX628TD57";
+            byte[] keybytes = Encoding.ASCII.GetBytes(encryptionkey.Length.ToString());
+            RijndaelManaged rijndaelCipher = new RijndaelManaged();
+            byte[] plainText = Encoding.Unicode.GetBytes(inputText.Replace("/", "ABDULLAH").Replace("==", "SALEH"));
+            PasswordDeriveBytes pwdbytes = new PasswordDeriveBytes(encryptionkey, keybytes);
+            using (ICryptoTransform encryptrans = rijndaelCipher.CreateEncryptor(pwdbytes.GetBytes(32), pwdbytes.GetBytes(16)))
+            {
+                using (MemoryStream mstrm = new MemoryStream())
+                {
+                    using (CryptoStream cryptstm = new CryptoStream(mstrm, encryptrans, CryptoStreamMode.Write))
+                    {
+                        cryptstm.Write(plainText, 0, plainText.Length);
+                        cryptstm.Close();
+                        return Convert.ToBase64String(mstrm.ToArray());
+                    }
+                }
+            }
+        }
+        public static string Decrypt(string encryptText)
+        {
+            string encryptionkey = "SAUW193BX628TD57";
+            byte[] keybytes = Encoding.ASCII.GetBytes(encryptionkey.Length.ToString());
+            RijndaelManaged rijndaelCipher = new RijndaelManaged();
+
+            byte[] encryptedData = Convert.FromBase64String(encryptText.Replace("ABDULLAH", "/").Replace("SALEH", "=="));
+            PasswordDeriveBytes pwdbytes = new PasswordDeriveBytes(encryptionkey, keybytes);
+            using (ICryptoTransform decryptrans = rijndaelCipher.CreateDecryptor(pwdbytes.GetBytes(32), pwdbytes.GetBytes(16)))
+            {
+                using (MemoryStream mstrm = new MemoryStream(encryptedData))
+                {
+                    using (CryptoStream cryptstm = new CryptoStream(mstrm, decryptrans, CryptoStreamMode.Read))
+                    {
+                        byte[] plainText = new byte[encryptedData.Length];
+                        int decryptedCount = cryptstm.Read(plainText, 0, plainText.Length);
+                        return Encoding.Unicode.GetString(plainText, 0, decryptedCount);
+                    }
+                }
+            }
+
+
+        }
+        static void Main(string[] args)
+        {
+            string Encrypts = Encrypt(DateTime.Now.ToString("yyyyMMddhhmmss"));
+            string Decrypts = Decrypt(Encrypts);
+
+            Console.WriteLine(Encrypts);
+            Console.WriteLine(Decrypts);
+
+        }
+    }
+}
